0975b3d679e6c5aa5c1622c7058e3eefd371ea06
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2013-07-12 17:07:01 -0400

    Reads cow files.

--- src/cowvis.c
+++ src/cowvis.c
@@ -5,15 +5,106 @@
 #include <GL/glu.h>
 #include "cowvis.h"
 
-#define ESCAPE 27
+#define ESCAPE_KEY 27
+
+int init_width = 640;
+int init_height = 480;
 
 int window = 0;
 int fullscreen = 0;
 float rotx = 0.0f;
 float roty = 0.0f;
+float transz = -3.0f;
+float delta_theta = 5.0f;
+float mag_scale = 1.1f;
 char window_name[] = "C.O.W. Vis";
 
-int create_window(char *title, int x, int y, int width, int height)
+cow_domain *dom;
+cow_dfield *dfield;
+
+void ReshapeFunc(int width, int height)
+{
+	if(height == 0)
+		height = 1;
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, ((GLfloat)width) / ((GLfloat)height), 0.1f, 100.0f);
+	
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void DisplayFunc()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	
+	glTranslatef(0.0f, 0.0f, transz);
+	glRotatef(roty, 0.0f, 1.0f, 0.0f);
+	glRotatef(rotx, 1.0f, 0.0f, 0.0f);
+	
+	glBegin(GL_QUADS);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f( 0.5f, 0.5f, 0.5f);
+	glVertex3f(-0.5f, 0.5f, 0.5f);
+	glVertex3f(-0.5f,-0.5f, 0.5f);
+	glVertex3f( 0.5f,-0.5f, 0.5f);
+	glEnd();
+	
+	glutSwapBuffers();
+}
+
+void KeyboardFunc(unsigned char key, int x, int y)
+{
+	switch(key)
+	{
+		case ESCAPE_KEY:	
+			cv_exit();
+			break;
+			
+		case 'f':
+			fullscreen = (fullscreen+1)%2;
+			glutDestroyWindow(window);
+			window = cv_create_window(window_name, 0, 0, init_width, init_height);
+			break;
+			
+		case '-':
+			transz *= mag_scale;
+			break;
+		case '=':
+			transz /= mag_scale;
+			break;
+		
+		default:
+			break;
+	}
+}
+
+void SpecialFunc(int key, int x, int y)
+{
+	switch(key)
+	{
+		case GLUT_KEY_RIGHT:
+			roty += delta_theta;
+			break;
+		case GLUT_KEY_LEFT:
+			roty -= delta_theta;
+			break;
+		case GLUT_KEY_UP:
+			rotx -= delta_theta;
+			break;
+		case GLUT_KEY_DOWN:
+			rotx += delta_theta;
+			break;
+		default:
+			break;
+	}
+	
+	glutPostRedisplay();
+}
+
+int cv_create_window(char *title, int x, int y, int width, int height)
 {
 	int id;
 	
@@ -30,17 +121,18 @@ int create_window(char *title, int x, int y, int width, int height)
 	if(fullscreen)
 		glutFullScreen();
 	
-	glutDisplayFunc(&DrawGLScene);
-	glutIdleFunc(&DrawGLScene);
-	glutReshapeFunc(&ReSizeGLScene);
-	glutKeyboardFunc(&keyPressed);
+	glutDisplayFunc(&DisplayFunc);
+	glutIdleFunc(&DisplayFunc);
+	glutReshapeFunc(&ReshapeFunc);
+	glutKeyboardFunc(&KeyboardFunc);
+	glutSpecialFunc(&SpecialFunc);
 	
-	InitGL(width, height);
+	cv_init_gl(width, height);
 	
 	return id;
 }
 
-void InitGL(int width, int height)
+void cv_init_gl(int width, int height)
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClearDepth(1.0f);
@@ -55,61 +147,59 @@ void InitGL(int width, int height)
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void ReSizeGLScene(int width, int height)
+void cv_exit()
 {
-	if(height == 0)
-		height = 1;
-	glViewport(0, 0, width, height);
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45.0f, ((GLfloat)width) / ((GLfloat)height), 0.1f, 100.0f);
-	
-	glMatrixMode(GL_MODELVIEW);
-}
-
-void DrawGLScene()
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	
-	glTranslatef(0.0f, 0.0f, -1.5f);
-	glRotatef(rotx, 0.0f, 1.0f, 0.0f);
-	glRotatef(roty, 1.0f, 0.0f, 0.0f);
-	
-	glBegin(GL_QUADS);
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex3f( 0.5f, 0.5f, 0.0f);
-	glVertex3f(-0.5f, 0.5f, 0.0f);
-	glVertex3f(-0.5f,-0.5f, 0.0f);
-	glVertex3f( 0.5f,-0.5f, 0.0f);
-	glEnd();
-	
-	glutSwapBuffers();
-}
-
-void keyPressed(unsigned char key, int x, int y)
-{
-	if(key == ESCAPE)
-	{
-		glutDestroyWindow(window);
-		exit(0);
-	}
-	
-	if(key == 'f')
-	{
-		fullscreen = (fullscreen+1)%2;
-		glutDestroyWindow(window);
-		window = create_window(window_name, 0, 0, 640, 480);
-	}
+	glutDestroyWindow(window);
+	cow_dfield_del(dfield);
+	cow_domain_del(dom);
+	cv_message("Everything swept under the rug.");
+	exit(0);
 }
 
 int main(int argc, char *argv[])
 {
+	int err;
+	char *filename;
+	char field_member[] = "phi/phi";
+	
+	if(argc > 1)
+		filename = argv[1];
+	else
+	{
+		printf("\nYa gotta give me a file numbnuts!\n\n");
+		return 0;
+	}
+	
+	dom = cow_domain_new();
+	dfield = cow_dfield_new();
+	
+	err = cow_domain_readsize(dom, filename, field_member);
+	if(err)
+	{
+		fprintf(stderr, "Unable to read size of dataset %s.  Does it even exist?\n", field_member);
+		return 1;
+	}
+	
+	cow_domain_setguard(dom, 2);
+	cow_domain_setextent(dom, 0, -0.5, 0.5);
+	cow_domain_setextent(dom, 1, -0.5, 0.5);
+	cow_domain_setextent(dom, 2, -0.5, 0.5);
+	cow_domain_commit(dom);
+	
+	cow_dfield_setname(dfield, "phi");
+	cow_dfield_addmember(dfield, "phi");
+	cow_dfield_addmember(dfield, "phiv");
+	cow_dfield_setdomain(dfield, dom);
+	cow_dfield_commit(dfield);
+	
+	cv_message("Reading data file");
+	
+	cow_dfield_read(dfield, filename);
+	cow_dfield_syncguard(dfield);
+	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
-	fullscreen = 0;
-	window = create_window(window_name, 0, 0, 640, 480);
+	window = cv_create_window(window_name, 0, 0, init_width, init_height);
 	glutMainLoop();
 
 	return 1;
--- src/cowvis.h
+++ src/cowvis.h
@@ -1,7 +1,16 @@
 #include "cow.h"
 
-int create_window(char *title, int x, int y, int width, int height);
+#define cv_message(msg) fprintf(stdout, "[cowvis] %s: %s\n", __FUNCTION__, msg);
+#define cv_error(msg) fprintf(stderr, "[ERROR] %s: %s\n", __FUNCTION__, msg);
+
+//Callback Functions
 void InitGL(int width, int height);
-void ReSizeGLScene(int width, int height);
-void DrawGLScene();
-void keyPressed(unsigned char key, int x, int y);
+void ReshapeFunc(int width, int height);
+void DisplayFunc();
+void KeyboardFunc(unsigned char key, int x, int y);
+void SpecialFunc(int key, int x, int y);
+
+//Everything Else
+int cv_create_window(char *title, int x, int y, int width, int height);
+void cv_init_gl(int width, int height);
+void cv_exit();
